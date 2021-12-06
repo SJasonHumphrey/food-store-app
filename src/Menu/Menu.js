30d202de3ae3e404dd1fbf9fdf202c4ccbf89287
SJasonHumphrey <sjasonhumphrey@gmail.com>
2021-12-06 12:55:14 -0600

    added photos and more styling to project

---
+++ src/Menu/Menu.js
@@ -0,0 +1,25 @@
+import { foodItems } from "../Data/FoodData";
+import React from "react";
+import styled from "styled-components";
+import { Food, FoodGrid, FoodLabel } from "./FoodGrid";
+
+const MenuStyled = styled.div`
+  height: 1000px;
+  margin: 0px 400px 50px 20px;
+`;
+
+export function Menu (){
+    return <MenuStyled>
+        <h1>Menu</h1>
+        <FoodGrid>
+        {foodItems.map(food => (
+            <Food img={food.img}>
+                <FoodLabel>
+                {food.name}
+                </FoodLabel>
+            </Food>
+        ))}
+        </FoodGrid>
+        </MenuStyled>
+
+}
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -9,12 +9,12 @@ const NavBarStyled = styled.div`
     width: 100%;
     position: fixed;
     z-index: 999;
-`
+`;
 const Logo = styled(Title)`
     font-size: 20px;
     color: white;
-    text-shadow: 1px 1px 4px #380502
-`
+    text-shadow: 1px 1px 4px #380502;
+`;
 
 export function NavBar(){
     return <NavBarStyled>
