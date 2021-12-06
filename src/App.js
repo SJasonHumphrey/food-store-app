e777ae388e01bbde735834f3bda503ce1d17bad9
SJasonHumphrey <sjasonhumphrey@gmail.com>
2021-12-06 15:06:46 -0600

    dialog function working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { GlobalStyle } from './styles/GlobalStyle'
+import { FoodDialog } from './FoodDialog/FoodDialog';
 import { NavBar } from './NavBar/NavBar';
 import { Banner } from './Banner/Banner';
 import { Menu } from './Menu/Menu';
 
 function App() {
+  const [openFood, setOpenFood] = useState();
   return (
     <>
     <GlobalStyle/>
+    <FoodDialog openFood={openFood} setOpenFood={setOpenFood}/>
     <NavBar />
     <Banner />
-    <Menu/>
+
+    <Menu setOpenFood={setOpenFood}/>
     </>
   );
 }
